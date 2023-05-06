102d6036f0ef86185126d7162ef4173640448eb3
JoseMariTan <132253808+JoseMariTan@users.noreply.github.com>
2023-05-06 23:37:55 +0800

    Add files via upload

---
+++ Coords.java
@@ -0,0 +1,13 @@
+package Finals;
+
+public class Coords {
+	private int desiredX = 0;
+
+	public int getDesiredX() {
+		return desiredX;
+	}
+
+	public void setDesiredX(int desiredX) {
+		this.desiredX = desiredX;
+	}
+}
