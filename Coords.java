dfbe0261138c70318fe4900ec0b10901fb428184
JoseMariTan <132253808+JoseMariTan@users.noreply.github.com>
2023-05-07 12:42:07 +0800

    Update Coords.java

--- Coords.java
+++ Coords.java
@@ -1,13 +1,139 @@
 package Finals;
 
 public class Coords {
-	private int desiredX = 0;
+	private int desiredXp1n1 = 0;
+	private int desiredXp1n2 = 0;
+	private int desiredXp1n3 = 0;
+	private int desiredXp1n4 = 0;
+	private int desiredXp1n5 = 0;
+	private int desiredXp2n1 = 0;
+	private int desiredXp2n2 = 0;
+	private int desiredXp2n3 = 0;
+	private int desiredXp2n4 = 0;
+	private int desiredXp2n5 = 0;
+	private int desiredXp3n1 = 0;
+	private int desiredXp3n2 = 0;
+	private int desiredXp3n3 = 0;
+	private int desiredXp3n4 = 0;
+	private int desiredXp3n5 = 0;
 
-	public int getDesiredX() {
-		return desiredX;
+	public int getDesiredXp1n1() {
+	    return desiredXp1n1;
 	}
 
-	public void setDesiredX(int desiredX) {
-		this.desiredX = desiredX;
+	public void setDesiredXp1n1(int desiredXp1n1) {
+	    this.desiredXp1n1 = desiredXp1n1;
+	}
+
+	public int getDesiredXp1n2() {
+	    return desiredXp1n2;
+	}
+
+	public void setDesiredXp1n2(int desiredXp1n2) {
+	    this.desiredXp1n2 = desiredXp1n2;
+	}
+
+	public int getDesiredXp1n3() {
+	    return desiredXp1n3;
+	}
+
+	public void setDesiredXp1n3(int desiredXp1n3) {
+	    this.desiredXp1n3 = desiredXp1n3;
+	}
+
+	public int getDesiredXp1n4() {
+	    return desiredXp1n4;
+	}
+
+	public void setDesiredXp1n4(int desiredXp1n4) {
+	    this.desiredXp1n4 = desiredXp1n4;
+	}
+
+	public int getDesiredXp1n5() {
+	    return desiredXp1n5;
+	}
+
+	public void setDesiredXp1n5(int desiredXp1n5) {
+	    this.desiredXp1n5 = desiredXp1n5;
+	}
+
+	public int getDesiredXp2n1() {
+	    return desiredXp2n1;
+	}
+
+	public void setDesiredXp2n1(int desiredXp2n1) {
+	    this.desiredXp2n1 = desiredXp2n1;
+	}
+
+	public int getDesiredXp2n2() {
+	    return desiredXp2n2;
+	}
+
+	public void setDesiredXp2n2(int desiredXp2n2) {
+	    this.desiredXp2n2 = desiredXp2n2;
+	}
+
+	public int getDesiredXp2n3() {
+	    return desiredXp2n3;
+	}
+
+	public void setDesiredXp2n3(int desiredXp2n3) {
+	    this.desiredXp2n3 = desiredXp2n3;
+	}
+
+	public int getDesiredXp2n4() {
+	    return desiredXp2n4;
+	}
+
+	public void setDesiredXp2n4(int desiredXp2n4) {
+	    this.desiredXp2n4 = desiredXp2n4;
+	}
+
+	public int getDesiredXp2n5() {
+	    return desiredXp2n5;
+	}
+
+	public void setDesiredXp2n5(int desiredXp2n5) {
+	    this.desiredXp2n5 = desiredXp2n5;
+	}
+
+	public int getDesiredXp3n1() {
+	    return desiredXp3n1;
+	}
+
+	public void setDesiredXp3n1(int desiredXp3n1) {
+	    this.desiredXp3n1 = desiredXp3n1;
+	}
+
+	public int getDesiredXp3n2() {
+	    return desiredXp3n2;
+	}
+
+	public void setDesiredXp3n2(int desiredXp3n2) {
+	    this.desiredXp3n2 = desiredXp3n2;
+	}
+
+	public int getDesiredXp3n3() {
+	    return desiredXp3n3;
+	}
+
+	public void setDesiredXp3n3(int desiredXp3n3) {
+	    this.desiredXp3n3 = desiredXp3n3;
+	}
+
+	public int getDesiredXp3n4() {
+	    return desiredXp3n4;
+	}
+
+	public void setDesiredXp3n4(int desiredXp3n4) {
+	    this.desiredXp3n4 = desiredXp3n4;
+	}
+
+	public int getDesiredXp3n5() {
+	    return desiredXp3n5;
+	}
+
+	public void setDesiredXp3n5(int desiredXp3n5) {
+	    this.desiredXp3n5 = desiredXp3n5;
 	}
 }
